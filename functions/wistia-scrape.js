07d4b331cc4d65dd9d5bafb9c02de724c61ca1f7
Ogunmoye Damilola <dami@ogunmoye.com>
2020-01-18 21:13:36 +0100

    update

--- functions/wistia-scrape.js
+++ functions/wistia-scrape.js
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
 
         let video = extractWistiaURL(element);
 
-        const browser = await chromium.puppeteer.launch({ // chromium
+        const browser = await chromium.puppeteer.launch({
             args: chromium.args,
             defaultViewport: chromium.defaultViewport,
             executablePath: await chromium.executablePath,
@@ -25,32 +25,9 @@ exports.handler = async (event, context) => {
 
         await page.goto(video.embedUrl, { waitUntil: ["domcontentloaded", 'networkidle2'] })
 
-        // let [el] = await page.$x('/html/body/script[4]');
-        
-        // const value = await el.getProperty('textContent');
-
-        // let valueTxt = await value.jsonValue();
-
-        // let url = (valueTxt.substring(valueTxt.indexOf('"url":"') + 1, valueTxt.indexOf('.bin"')) + '.mp4')
-        //         .replace('url":"', '');
-
         let url = await getWistiaDownloadUrl(page, '/html/body/script[4]');
 
-
         url = url !== '.mp4' ?  url : await getWistiaDownloadUrl(page, '/html/body/script[5]');
-        // if(url === '.mp4') {
-
-        //     url = await getWistiaDownloadUrl(page, '/html/body/script[4]');
-            // const [el] = await page.$x('/html/body/script[5]');
-
-            // const value2 = await el2.getProperty('textContent');
-
-            // let valueTxt2 = await value2.jsonValue();
-
-            // url = (valueTxt2.substring(valueTxt2.indexOf('"url":"') + 1, valueTxt2.indexOf('.bin"')) + '.mp4')
-            //     .replace('url":"', '');
-        // }
-
 
         result = { ...video, url }
 
@@ -89,11 +66,11 @@ async function getWistiaDownloadUrl(page, xpath) {
 
 
 function extractWistiaURL(element) {
-    const imgRegex = /<img[^>]+src="([^">]+)/;
+    let imgRegex = /<img[^>]+src="([^">]+)/;
 
     return {
         title: element.substring(element.lastIndexOf('">') + 1, element.lastIndexOf('</a></p>')).replace('>', ''),
-        image: imgRegex.exec(element) !== null? imgRegex.exec(element)[2] : null,
-        embedUrl: "https://fast.wistia.net/embed/iframe/" +element.substring(element.indexOf('?wvideo=') + 1, element.indexOf('">')).replace('wvideo=', '')
+        embedUrl: "https://fast.wistia.net/embed/iframe/" +element.substring(element.indexOf('?wvideo=') + 1, element.indexOf('">')).replace('wvideo=', ''),
+        image: imgRegex.exec(element) !== null ? imgRegex.exec(element)[1] : null
     };
 }    
--- public/script.js
+++ public/script.js
@@ -22,8 +22,21 @@ document.querySelector('button[type="submit"]').addEventListener('click', (e) =>
         .then((res) => res.json())
         .then((res) => {
 
-
+            
             console.log(res)
+
+            let card = `<div class="card">
+                <div class="card-image">
+                    <img src="${res.image}" alt="${res.title}">
+                </div>
+                <div class="card-body">
+                    <h1>${res.title}</h1>
+                    <div class="card-buttons">
+                        <button type="button" onclick="${downloadVideo(res.url)}">Download</button>
+                        <button type="button" onclick="${copyEmbed(res.embedUrl)}">Copy Embed</button>
+                    </div>
+                </div>
+            </div>`;
 
 
 
@@ -32,10 +45,18 @@ document.querySelector('button[type="submit"]').addEventListener('click', (e) =>
 
             // const img = document.createElement('img');
             // img.src = bufferToImageUrl(res.buffer.data);
-            document.getElementById('result').innerHTML = '<pre>' + JSON.stringify(res) + '</pre>' ;
+            document.getElementById('result').append(card);
         })
         .catch((err) => {
             console.log(err)
             document.getElementById('result').textContent = `Error: ${err.toString()}`
         });
 });
+
+function downloadVideo(url) {
+    console.log('Downloading ' + url)
+}
+
+function copyEmbed(url) {
+    console.log('Copied embed code to clipboard' + url)
+}
